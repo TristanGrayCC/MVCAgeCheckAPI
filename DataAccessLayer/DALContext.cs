2a95d55f919125326bfcb8790c2d641649994aba
Tristan Gray <tristangraycc@gmail.com>
2019-04-25 20:28:32 +0100

    Initial setup

---
+++ DataAccessLayer/DALContext.cs
@@ -0,0 +1,77 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class DALContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Name = "Jaffa Clementine",
+                Description = "12 easy peel clementines",
+                Category = "Fruit"
+            },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                },
+                new Product
+                {
+                    Name = "Jaffa Clementine",
+                    Description = "12 easy peel clementines",
+                    Category = "Fruit"
+                }
+                );
+        }
+    }
+}
