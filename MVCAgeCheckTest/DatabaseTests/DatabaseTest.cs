d9d152ef5f435eb4a095c855a2648abc80cbd9ac
Tristan Gray <tristangraycc@gmail.com>
2019-05-16 22:30:25 +0100

    Test update

---
+++ MVCAgeCheckTest/DatabaseTests/DatabaseTest.cs
@@ -0,0 +1,119 @@
+﻿using DotNetCoreAPI;
+using DotNetCoreAPI.Dtos;
+using DotNetCoreAPI.Models;
+using DotNetCoreAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace APITest.DatabaseTests
+{
+    public class DatabaseTest
+    {
+        [Fact]
+        public void CreateProduct_AddsToDatabase()
+        {
+            var options = new DbContextOptionsBuilder<DALContext>()
+                .UseInMemoryDatabase(databaseName: "CreateProductAddsToDatabase")
+                .Options;
+
+            var dateTime = new DateTime(2019, 01, 01);
+            var fruit = "Strawberries";
+
+            var dateToSearch = new Login
+            {
+                DateTime = dateTime
+            };
+
+            // Run the test against one instance of the context
+            using (var context = new DALContext(options))
+            {
+                var service = new UserService(context);
+                service.CreateLogin(new LoginDto()
+                {
+                    DateTime = dateToSearch.DateTime
+                });
+            }
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using (var context = new DALContext(options))
+            {
+                Assert.Equal(1, context.Users.Count());
+                Assert.Equal(fruit, context.Users.Single().Name);
+            }
+        }
+
+        [Fact]
+        public void GetAllProductsByCategory_ReturnsProductsForCategory()
+        {
+            var options = new DbContextOptionsBuilder<DALContext>()
+                .UseInMemoryDatabase(databaseName: "GetAllProductsByCategory")
+                .Options;
+
+            var doB = new DateTime(2019, 01, 01);
+            var dateTime = new DateTime(2019, 01, 01);
+            var userNameToSearch = "Name";
+
+            var listOfUserLogins = new List<DateTime>
+            {
+                new DateTime(2019, 01, 01), new DateTime(2019, 01, 02), new DateTime(2019, 01, 03)
+            };
+            var notUserListOfLogins = new List<DateTime>
+            {
+                new DateTime(2019, 01, 04), new DateTime(2019, 01, 05)
+            };
+
+            var user = new User
+            {
+                Name = userNameToSearch,
+                DateOfBirth = doB
+            };
+            var anotherUser = new User
+            {
+                Name = "Not this",
+                DateOfBirth = doB
+            };
+
+            var allLogins = new List<Login>();
+
+            foreach (var loginTime in listOfUserLogins)
+            {
+                allLogins.Add(new Login
+                {
+                    DateTime = loginTime,
+                    User = user
+                });
+            }
+
+            foreach (var loginTime in notUserListOfLogins)
+            {
+                allLogins.Add(new Login
+                {
+                    DateTime = loginTime,
+                    User = anotherUser
+                });
+            }
+
+            using (var context = new DALContext(options))
+            {
+                context.Users.Add(user);
+                context.Users.Add(anotherUser);
+
+                foreach (var login in allLogins)
+                {
+                    context.Logins.Add(login);
+                }
+                context.SaveChanges();
+            }
+
+            using (var context = new DALContext(options))
+            {
+                var service = new UserService(context);
+                var result = service.GetAllLoginsByUser(userNameToSearch);
+                Assert.Equal(3, result.Count());
+            }
+        }
+    }
+}
